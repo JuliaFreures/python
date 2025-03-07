280dbf6944c10168e65d7f75b908f9ccd469c36f
Julia Freures <juliafreures0111@gmail.com>
2025-03-07 19:06:15 -0300

    n

---
+++ ex004/par.py
@@ -0,0 +1,7 @@
+
+def eh_par(numero):
+    return numero % 2 == 0
+print(eh_par(45))
+print(eh_par(78))
+print(eh_par(170))
+print(eh_par(1695))
---
+++ ex006/return.py
@@ -0,0 +1,5 @@
+def soma(a , b):
+    return a + b
+print(soma(3,5))
+print(soma(8,39))
+print(soma(759, 6658))
