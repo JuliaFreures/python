e1187174c761a02a131e8ba6f6eeed0b5143d13d
Julia Freures <juliafreures0111@gmail.com>
2025-03-08 19:37:37 -0300

    mu

--- ex011/enquanto.py
+++ ex011/enquanto.py
@@ -1,5 +1,8 @@
 nota =  float(input("Digite uma nota:"))
-if nota >= 0 and nota <=10:
+#if nota >= 0 and nota <= 10:
+#  print("nota")
+#else:
+#    print("Nota invalida")
+while nota< 0 or nota > 10:
+    nota = float(input("Digite um valor VALIDO de nota:"))
     print(nota)
-else:
-    print("nota invalida")
---
+++ ex011/experimentos.py
@@ -0,0 +1,16 @@
+#n = 10 
+#while n >= 0:
+#    print(n)
+#    n = n-1
+#    if n == 5:
+#        break
+n = 10 
+while n >= 0:
+    if n % 2 == 1:
+        n = n - 1
+        continue
+    print(n)
+    n = n - 1
+    
+    
+        
