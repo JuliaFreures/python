fe0b03b1299528bef1f112ed075a21c5c75c2779
Julia Freures <juliafreures0111@gmail.com>
2025-03-12 14:13:49 -0300

    string

---
+++ ex012/string.py
@@ -0,0 +1,24 @@
+texto = "matematica"
+#acesso por indice
+#print(texto[-1])
+#print(texto[len(texto)-1])
+#print("-------------")
+
+#repetiçao com for
+#for c in texto:
+ #   print(c)
+ #  print("-------------") 
+ 
+ # comparação entre string 
+print("matematica" == texto)
+print(texto.upper())
+print("matematica". upper() == texto. upper())
+print(texto.lower())
+print("matematica". lower() == texto. lower())
+
+#verificar se está correta
+frase = "A nota de matematica de pedro foi 7.8"
+if texto. lower() in frase.lower():
+    print("achou", texto)
+else:
+    print("não está aqui")
