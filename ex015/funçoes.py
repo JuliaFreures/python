8227ebaeff7845a6b83410915669b75c109e6e67
Julia Freures <juliafreures0111@gmail.com>
2025-03-13 14:26:25 -0300

    c

---
+++ ex015/funçoes.py
@@ -0,0 +1,15 @@
+num = int(input("Digite um numero:"))
+num = num + 3
+print (num)
+
+
+def multiplicar_por_7(n):
+    res = 7 * n
+    return res
+
+def media (a, b):
+    return(a + b) / 2
+
+#r = multiplicar_por_7(10)
+#print(f"Resultado é {r}")
+print(media(10, 8))
