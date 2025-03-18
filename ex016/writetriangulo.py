9b93b029faf96e2502ea71fb748e73aee429a83e
Julia Freures <juliafreures0111@gmail.com>
2025-03-18 16:06:29 -0300

    python

--- ex016/writetriangulo.py
+++ ex016/writetriangulo.py
@@ -1,27 +1,21 @@
-def triangulo_valido(a, b, c):
-    if a <= 0 or b <= 0 or c <=0:
-        return False
-    if a > (b + c):
-        return False
-    if b > (a + c):
-        return False
-    if c > (a + b):
-        return False
-    return True
+import trianglos as tri
 
 with open ("data/lados.txt", "r")as arq:
     linhas = arq.readlines()
-    respostas =[]
-    for linha in linhas:
+respostas =[]
+for linha in linhas:
       partes =  linha.strip().split()
-      for i in range (len(partes)):
-          partes [i] = float(partes[i])
-          respostas = triangulo_valido(partes[0], partes[1], partes[2])
-          respostas.append(respostas)
-    print(respostas)
 
-    with open("resposta-triangulos.txt", "w")as arq:
-        for resp in respostas:
-            arq.write(f"{resp}\n")
+      for i in range (len(partes)):
+          partes[i] = float(partes[i])
+      resposta = tri.triangulo_valido(partes[0], partes[1], partes[2])
+      
+      if resposta:
+        respostas.append("Sim")
+      else:
+        respostas.append("False")
+with open("resposta-triangulos.txt", "w")as arq:
+    for resp in respostas:
+     arq.write(f"{resp}\n")
         
     
