4c7617eba55353549512c1b16e86925a164a75fb
Julia Freures <juliafreures0111@gmail.com>
2025-03-26 13:09:50 -0300

    att

---
+++ ex022/bradesco.py
@@ -0,0 +1,9 @@
+def notas():
+    n = float(input("Digite sua nota:"))
+    return n
+
+def resultado(n1, n2):
+    media = (n1 + n2) / 2
+    print("Nota 1:", n1)
+    print("Nota 2:", n2)
+    print("Média:", media, "Resultado:", and ="")
